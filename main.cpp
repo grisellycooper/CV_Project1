6e9a4735edd747facbe9013d3c28216be7b8f1cc
Griselly Cooper <griselly.ramos.c@gmail.com>
2018-12-04 12:11:13 -0500

    Draw center points

--- main.cpp
+++ main.cpp
@@ -120,8 +120,8 @@ int main(int argc, char** argv){
                         if(distance < threshold){
                             minEllipseSelected.push_back(minEllipse[i]);            
                             minEllipseSelected.push_back(minEllipse[hierarchy[i][2]]);                            
-                            ellipse(frame, minEllipse[i], Scalar(0,0,255), 2, 8 );            
-                            ellipse(frame, minEllipse[hierarchy[i][2]], Scalar(0,0,255), 1, 8 );  
+                            //ellipse(frame, minEllipse[i], Scalar(0,0,255), 2, 8 );            
+                            //ellipse(frame, minEllipse[hierarchy[i][2]], Scalar(0,0,255), 1, 8 );  
                             centers.push_back(Point2f(child_c_x,child_c_y));          
                         }                                                        
                     }
@@ -130,7 +130,12 @@ int main(int argc, char** argv){
                 
                 break;        
         }
+        // The size grid must be "parametrizado"
         if(centers.size() == 30){
+            // Draw center points
+            for(int i = 0; i < centers.size();i++)
+                circle(frame, centers[i],1,Scalar(0,0,255),4,8);
+
             namedWindow("Video Display", WINDOW_NORMAL);
             imshow("Video Display", frame);
             waitKey(20); // waits to display frame            
